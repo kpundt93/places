8d2deccc6f3f0f0b97378730149f39de00b4d0b6
Katie Pundt <kzpundt@gmail.com>
2021-11-01 14:42:38 -0700

    add jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,28 @@ function Location(name, landmarks, time, notes) {
   this.time = time;
   this.notes = notes;
 }
+
+Destination.prototype.findLocation = function(id) {
+  if(this.locations[id] !=undefined) {
+    return this.locations[id];
+  }
+  return false;
+};
+
+// UI Logic
+
+$(document).ready(function() {
+  $("#new-location").submit(function(event) {
+    event.preventDefault();
+
+    const location = $("#location").val();
+    const landmark = $("#landmark").val();
+    const time = $("#time").val();
+    const notes = $("#notes").val();
+
+    $("#output").append("<li>" + location + "</li>");
+    $("#output").append("<li>" + landmark + "</li>");
+    $("#output").append("<li>" + time + "</li>");
+    $("#output").append("<li>" + notes + "</li>");
+  });
+});
